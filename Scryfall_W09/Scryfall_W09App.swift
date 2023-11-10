e98519aec7b753831dfad8298f157169c6ce82d6
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-11-10 08:01:57 +0700

    Initial Commit

---
+++ Scryfall_W09/Scryfall_W09App.swift
@@ -0,0 +1,17 @@
+//
+//  Scryfall_W09App.swift
+//  Scryfall_W09
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Scryfall_W09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
